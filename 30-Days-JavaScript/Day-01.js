9fd50244edd2fe04222435b8c569f6a35ca7264b
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-07-16 00:22:42 +0500

    Day-01 of 30-Days-Challenge completed

---
+++ 30-Days-JavaScript/Day-01.js
@@ -0,0 +1,48 @@
+//Activity 01 Declare let and var
+     //Task #01  Variable Declare var assign a number and log it.
+         var number1 = 456;
+         // console.log(number1);
+
+    //Task #02  Variable Declare let assign a number and log it.
+        let number2=897;
+        // console.log(number2);
+
+ 
+//Activity 02 Constant Declare
+     // Task#03 a const assign a number and log it.
+      const number3=786;
+     // console.log(number3);
+
+//Activity 03 Data Types
+     //Task#04 Use all date types and check diff types.
+     const name="shoaib";
+     let grade='A';
+     let salary=12.34;
+     let bol=true;
+     let arr=[1,'3',8,5,6,4,8,9];
+     let obj={name:"shoaib",age:23};
+
+    //  console.log(typeof (name));
+    //  console.log(typeof (grade));
+    //  console.log(typeof salary);
+    //  console.log(typeof bol);
+    //  console.log(typeof arr);
+    //  console.log(typeof obj);
+    //  console.log(typeof number1);  
+     
+//Activity 04 Reassign value
+    //Task05 Assign let keyword with a number/value and reassign it with another value. 
+   let value=1234;
+//    console.log(value);
+
+   value=897;
+//    console.log(value);      
+
+//Activity 05 Reassign value
+    //Task05 Assign const keyword with a number/value and reassign it with another value and see errors
+    const value1=1234;
+    // console.log(value);
+ 
+    // value1=897;
+    // console.log(value);     
+    :arrow_upper_right:
