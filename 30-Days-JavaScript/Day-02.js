ac89f666f8f3980b2ddfca17eb8ca3d6e81b6181
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-07-17 14:01:48 +0500

    day-02 Done

---
+++ 30-Days-JavaScript/Day-02.js
@@ -0,0 +1,74 @@
+//Task #01  Arithmetic Operators
+           //Activity #01  Add two number 
+                    let number1=12;
+                    var number2=14;
+                    //let number3=12;
+
+                    //console.log(number1 + number2);
+           
+                    //Activity #02 Subtract two number 
+                     //console.log(number1 - number2);
+                    
+                   //Activity #03 Subtract two number
+                   // console.log(number1 - number2);
+                      
+                   //Activity #04 Multiply two number
+                  //  console.log(number1 * number3);
+                     
+                   //Activity #05 Divide two number
+                   // console.log(number1 / number2);
+                   
+                  //Activity #06 Find the Reminder of two number
+                  // console.log(number1 % number3);
+                    
+//Task #02  Assignment Operators
+                   //Activity #07  Use += operator add a number  from variable 
+                   //console.log(number1 += number2);
+
+                   //Activity #08  Use += operator subtract a number  from variable 
+                   //console.log(number1 += number2);
+                    
+//Task #03  Comparison Operators
+           
+                    //Activity #09 use > and < to compare two number and log the result
+                    // console.log(number1 > number2);
+                    // console.log(number1 < number2);
+
+
+                   //Activity #10 use >= and <= to compare two number and log the result
+                   //console.log(number1 >= number2);
+                   //console.log(number1 <= number2);
+                   
+          
+                   //Activity #11 use == and === to compare two number and log the result
+                   //console.log(number1 == number2);
+                  // console.log(number1 === number2);
+
+//Task #04 Logical operators
+
+                   //Activity #12 Use && operator to combine two condition and log its result.
+                   //console.log(number1 > number2 && number1 < number2);
+                   //console.log(number1 > number2 && number1 <= number2);
+                   //console.log(number1 < number2 && number1 <= number2);
+                   //console.log(number1 >= number2 && number1 > number2);
+                   //console.log(number1 < number2 && number1 < number2);
+
+
+                   //Activity #13 Use || operator to combine two condition and log its result.
+                   // console.log(number1 > number2 || number1 < number2);
+                   //console.log(number1 > number2 || number1 <= number2);
+                   //console.log(number1 < number2 || number1 <= number2);
+                   //console.log(number1 >=number2 || number1 > number2);
+                   //console.log(number1 < number2 || number1 < number2);
+
+                   //Activity #14 Use ! operator negate a condition and log to show it.
+                   //console.log(number1 != number2 );
+                   //console.log(number1 !== number2 );
+                   //console.log(number1 != number2 );
+                    
+//Task #05  Ternary Operator 
+                  //Activity #15 Write a program that use ternary operator to check if it is positive or not.
+                    let number4=0;
+                    console.log(number4 > 0 ? "Positive" : "Negative");
+
+
