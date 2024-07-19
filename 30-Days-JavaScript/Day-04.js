8f77fc19066a1e7d997f93b81f741b697e0c40fc
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-07-19 17:39:28 +0500

    Day-04 done

--- 30-Days-JavaScript/Day-04.js
+++ 30-Days-JavaScript/Day-04.js
@@ -6,7 +6,7 @@
        //Activity#02  Print the multiplication table of 5.
                    let table=5;
        for (let i = 1; i <=10; i++) {
-                        
+                         
         console.log(`5 * ${i} =  ${table * i}`);
   }
          
