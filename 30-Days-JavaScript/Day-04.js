02c569f1e0339dae790a9eb23c7e84c0e1445e28
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-07-19 17:38:48 +0500

    Day-04 done

---
+++ 30-Days-JavaScript/Day-04.js
@@ -0,0 +1,101 @@
+//-------------------Task#01 For Loop------------------------------
+       //Activity#01  Print list of number from 1 to 10.
+                  for (let i = 0; i <=10; i++) {
+                        console.log(i);
+                  }
+       //Activity#02  Print the multiplication table of 5.
+                   let table=5;
+       for (let i = 1; i <=10; i++) {
+                        
+        console.log(`5 * ${i} =  ${table * i}`);
+  }
+         
+
+//-------------------Task#02 While Loop----------------------------
+       //Activity#03    calculate the sum of 1 to 10 using 
+                          /*  let sum=0;
+                               let i=0;
+                               let num=10
+                         while (i <=num) {
+                               sum += i;
+                                i++;
+                         }
+                         console.log(sum);
+                         */
+                         
+       //Activity#04    Print number from 10 to 1 using while loop
+       /*
+       let num = 10;
+
+       while (num > 0) {
+           console.log(num);
+           num--;
+       }
+       */
+                         
+//-------------------Task#03 Do while Loop--------------------------
+       //Activity#05    print 1 to 5 using do....while loop
+                    /*    let number=1;
+                      do {
+                              console.log(number);
+                              number++;
+                        }
+                        while (number<=5);*/
+       //Activity#06    calculate the factorial of a number
+                   /* var number=1;
+                      let fact=1;
+                    do {
+                 
+                        fact *=number;
+                         number++;
+                      }
+                     while (number<=5);
+                      console.log(fact);
+       */
+//-------------------Task#04 Nested Loop----------------------------
+       //Activity#07    print the following pattern
+         /*               
+                          *
+                          **
+                          ***
+                          ****    
+                          *****    
+         */          
+                          /*        let n=5;
+                           for (let i = 0; i <=n; i++) {
+                              let stars="";
+                            for(let j=0; j<i; j++)
+                               {
+                                  stars +="*";
+                               }   
+                               console.log(stars);                     
+                           }
+                           */       
+
+
+
+//-------------------Task#05 Loop Control statement------------------
+       //Activity#08   Print Number from 1 to 10 but skip 5 using continue 
+              /*   let number=10;
+                 for (let i = 0; i <=number; i++) {
+                       if(i===5)
+                       {
+                        continue;
+                       }
+                          console.log(i);
+                 }
+                          */
+       //Activity#09   Print the number 1 to 10 but stop when 7 come using break statement
+                  /*
+                        let number=10;
+                    for (let i = 0; i <=number; i++) {
+                       if(i===7)
+                       {
+                        break;
+                       }
+                          console.log(i);
+                        }
+
+                        */
+    
+                      
