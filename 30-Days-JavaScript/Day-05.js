ac892ced483f4c6d0e5cc76f819c3cae7379f480
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-07-20 23:38:33 +0500

    Day-05 half done

--- 30-Days-JavaScript/Day-05.js
+++ 30-Days-JavaScript/Day-05.js
@@ -1,10 +1,34 @@
 //--------------Activity01 Function Declaration--------------------------------
                //Task#01 check the number is even or odd and consol it.
+                 /* let number=13;
+                  function check_even_odd() {
+                        if(number % 2 === 0)
+                             console.log("Even");
+                        else
+                           console.log("Odd");
+                  }
+                  check_even_odd();*/
                // Activity02 calculate the square if number.
-
-
+                     /*   let Number=13;
+                       function square_number() {
+                             console.log(Number*Number);
+                       }
+                          square_number();
+                          */
 //--------------Activity02 Function Expression---------------------------------
                //Activity03  find the maximum of two number 
+                 /* let Number=13;
+                  let number2=13;
+                  function max_number() {
+                            if(Number>number2)
+                                console.log(`${Number} is greater.`) 
+                            else if(Number<number2)
+                                console.log(`${number2} is greater.`) 
+                            else
+                                console.log("Both are equal.")
+              }
+                 max_number();
+                 */
                //Activity04  concat two string an show the result.  
                   
 
@@ -21,5 +45,5 @@
 //--------------Activity05 High Order Function----------------------------------
                //Activity09  Take a function and a Number and calls that many times.
                //Activity10  Two function and a value, applies the first function to the value and then applies the second function result.
-               
+
                  
