8925302cbef57fdb151240c11ab99413eb974b35
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-07-20 20:56:56 +0500

    Adding Day-05 task/activities

---
+++ 30-Days-JavaScript/Day-05.js
@@ -0,0 +1,25 @@
+//--------------Activity01 Function Declaration--------------------------------
+               //Task#01 check the number is even or odd and consol it.
+               // Activity02 calculate the square if number.
+
+
+//--------------Activity02 Function Expression---------------------------------
+               //Activity03  find the maximum of two number 
+               //Activity04  concat two string an show the result.  
+                  
+
+//--------------Activity03 Arrow Function --------------------------------------
+               //Activity05  calculate the sum of two number
+               //Activity06  check if a string contains char and show bool value
+                
+
+//--------------Activity04 Function Parameters and Default values---------------
+               //Activity07  Take two parameters and product of them to show result
+               //Activity08  take person name and age show greetings msg.
+                
+
+//--------------Activity05 High Order Function----------------------------------
+               //Activity09  Take a function and a Number and calls that many times.
+               //Activity10  Two function and a value, applies the first function to the value and then applies the second function result.
+               
+                 
