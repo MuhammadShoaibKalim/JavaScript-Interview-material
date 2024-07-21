ef39fb26a09bb027616b66104c603012d1e7a49a
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-07-21 05:42:36 +0500

    Day-05 done

--- 30-Days-JavaScript/Day-05.js
+++ 30-Days-JavaScript/Day-05.js
@@ -59,15 +59,47 @@
             
             console.log(isStringContainsChar(myString, myChar)); // Outputs: true
             ;*/
-
+            /*
+              const stringContain = ( str, char)=>{
+                      return str.includes(char)
+              } 
+              const str="Muhammad Shoaib kalim";
+              const ch="S";
+              console.log(stringContain(str, ch));
+                   */
 //--------------Activity04 Function Parameters and Default values---------------
                //Activity07  Take two parameters and product of them to show result
-              
+                        /*  function return_sum(num1, num2) {
+                                return (num1+num2);
+                          }
+                          console.log(return_sum(14,35));
+                          */
                //Activity08  take person name and age show greetings msg.
-                
-
+ /*
+               function greetings(name, age) {
+                            return (`Hello, I have ${name} and it is ${age} old.`)
+                         }
+                          console.log(greetings("HP", 3));
+*/
 //--------------Activity05 High Order Function----------------------------------
                //Activity09  Take a function and a Number and calls that many times.
+                       /* function print_hello(num) {
+                            for(let i=0; i<num; i++) {
+                                  console.log("Hello");
+                            }
+                          }
+                          print_hello(5);
+                          */
+                          /*
+                        const numbers=[12,34,11,15,17,19];
+                        const double = numbers.map( num => num*2);
+                        console.log(double);
+                          
+                        const find = numbers.map( num => num>15 );
+                        console.log(find);
+                           */
                //Activity10  Two function and a value, applies the first function to the value and then applies the second function result.
-
-                 
+                            const multiply =(num, multiplier) =>num * multiplier;
+                            const add = (add, addition) => add + addition;
+                            const value=12;
+                            console.log(add(multiply(value, 2),6));
