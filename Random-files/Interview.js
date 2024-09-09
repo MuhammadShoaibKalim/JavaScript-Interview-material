fdea126a001ae8599ad1a1356542fcbbf04d5ad0
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-09-09 16:10:10 +0500

    js Interview preparation starts

---
+++ Random-files/Interview.js
@@ -0,0 +1,16 @@
+console.log("!------------Here we are practicing JS Interview Question------------!");
+
+// Question: Write a function to check if a given number is prime or not.
+/*function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(` Yes, it , ${isPrime(17)},  The given number, ${num}, is prime`);*/
